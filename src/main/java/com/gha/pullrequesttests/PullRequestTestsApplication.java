28a7894b6d747bf7e096fef0fc322d26768e2ec6
David <david.sannicolas@gmail.com>
2024-01-19 17:20:21 +0100

    initial java project

---
+++ src/main/java/com/gha/pullrequesttests/PullRequestTestsApplication.java
@@ -0,0 +1,13 @@
+package com.gha.pullrequesttests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gha/pullrequesttests/PullRequestTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gha.pullrequesttests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
